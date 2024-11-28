c7c4cc8ae173c6013514c857b31e93d07da6cc1c
Marcello <hey@marcello.gg>
2024-11-27 19:23:53 -0500

    feature: support older browsers

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 air -c ./.air.toml & \
-npx tailwind -i './public/styles.css' -o './public/styles-local.min.css' --watch
+npx tailwindcss -i './public/styles.css' -o './public/styles-local.min.css' --watch
